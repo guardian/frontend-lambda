792047fe1d12a3cc865647f5b3351b3ea660f7a6
Jon Soul <jonathan.soul@guardian.co.uk>
2016-07-29 10:50:25 +0100

    Enable lambda for PROD (once trigger is enabled)

--- facia-purger/src/main/scala/com/gu/purge/facia/Lambda.scala
+++ facia-purger/src/main/scala/com/gu/purge/facia/Lambda.scala
@@ -10,7 +10,7 @@ import scala.collection.JavaConverters._
 
 class Lambda() extends RequestHandler[S3Event, Boolean] with Logging {
 
-  var stage = "DEV" // TODO
+  var stage = "PROD"
   private lazy val httpClient = new OkHttpClient()
 
   override def handleRequest(event: S3Event, context: Context) = {
