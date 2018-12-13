f6207373354457db29fe4ef5ed8e378ba687a554
Philip McMahon <philip.mcmahon@guardian.co.uk>
2018-12-13 15:03:18 +0000

    Add stage from environment variable.

--- facia-purger/src/main/scala/com/gu/purge/facia/Lambda.scala
+++ facia-purger/src/main/scala/com/gu/purge/facia/Lambda.scala
@@ -10,7 +10,7 @@ import scala.collection.JavaConverters._
 
 class Lambda() extends RequestHandler[S3Event, Boolean] with Logging {
 
-  var stage = "PROD"
+  var stage = Option(System.getenv("Stage")).getOrElse("PROD")
   private lazy val httpClient = new OkHttpClient()
 
   override def handleRequest(event: S3Event, context: Context) = {
