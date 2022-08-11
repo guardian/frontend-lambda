0e227aa3fd354356a9ae433efd9416a984b1886c
Julia Branke <Julia.Branke@guardian.co.uk>
2022-08-11 15:10:28 +0100

    Replaced CfnInclude with GuLamdiaFunction

--- cdk/lib/facia-purger.ts
+++ cdk/lib/facia-purger.ts
@@ -1,14 +1,18 @@
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
 import type { App } from 'aws-cdk-lib';
-import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 
 export class FaciaPurger extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
 
-		new CfnInclude(this, 'newFaciaPurgerId', {
-			templateFile: '../facia-purger/cloudformation.yaml',
+		new GuLambdaFunction(this, 'faciaPurgerLambda', {
+			app: 'facia-purger',
+			fileName: 'facia-purger.jar',
+			runtime: Runtime.JAVA_11,
+			handler: 'com.gu.purge.facia.Lambda',
 		});
 	}
 }
