148950745d551fe8a7305a2208ef77ac1979fdd6
Max Duval <hi@mxdvl.com>
2022-09-12 15:27:19 +0100

    chore: lint & format files

--- cdk/lib/facia-purger.ts
+++ cdk/lib/facia-purger.ts
@@ -1,15 +1,14 @@
-import { join } from 'path';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import type { App } from 'aws-cdk-lib';
 import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
 
-
 export class FaciaPurger extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
 
-		new CfnInclude(this, "newFaciaPurgerId", {templateFile:"../facia-purger/cloudformation.yaml"} )
-
+		new CfnInclude(this, 'newFaciaPurgerId', {
+			templateFile: '../facia-purger/cloudformation.yaml',
+		});
 	}
 }
