bc987bc1fb855bd8c253a7b612215cd1cb9afcfb
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2023-03-16 14:44:07 +0000

    backup-paramater-store cdk

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,7 +1,13 @@
 import 'source-map-support/register';
 import { App } from 'aws-cdk-lib';
 import { FaciaPurger } from '../lib/facia-purger';
+import {BackupParamaterStore} from "../lib/backup-paramater-store";
 
 const app = new App();
 new FaciaPurger(app, 'FaciaPurger-CODE', { stack: 'frontend', stage: 'CODE' });
 new FaciaPurger(app, 'FaciaPurger-PROD', { stack: 'frontend', stage: 'PROD' });
+new BackupParamaterStore(app, 'BackupParamaterStore-CODE', {
+	stack: 'frontend',
+	stage: 'CODE',
+});
+// new BackupParamaterStore(app, 'FaciaPurger-PROD', { stack: 'frontend', stage: 'PROD' });
