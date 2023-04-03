d37b016a1f2fe6b3497b641cee59ee372d2d9a9d
DanielCliftonGuardian <110032454+DanielCliftonGuardian@users.noreply.github.com>
2023-04-03 14:14:17 +0100

    update backup-parameter-store.ts

--- cdk/lib/backup-parameter-store.ts
+++ cdk/lib/backup-parameter-store.ts
@@ -1,14 +1,31 @@
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import type { App } from 'aws-cdk-lib';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
+import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
 
 export class BackupParameterStore extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
 
-		new CfnInclude(this, 'newBackupParameterStoreId', {
-			templateFile: '../backup-parameter-store/cfn.yaml',
+		const lambda = new GuLambdaFunction(this, 'backupParameterStore', {
+			app: 'backup-parameter-store',
+			fileName: 'backup-parameter-store.jar',
+			runtime: Runtime.JAVA_11,
+			handler: 'com.gu.backupparameterstore.Lambda',
+			environment: {
+				Stage: this.stage,
+			},
+			memorySize: 512,
 		});
+		this.overrideLogicalId(lambda, {
+			logicalId: 'Lambda',
+			reason: 'preserve existing triggers',
+		});
+
+		// new CfnInclude(this, 'newBackupParameterStoreId', {
+		// 	templateFile: '../backup-parameter-store/cfn.yaml',
+		// });
 	}
 }
